f73613a542e086f6312b6a4a31de68e7820918ed
DanielmGomes <danielmoreiragomes.dmg@gmail.com>
2019-06-16 17:04:28 -0300

    pagina inicial

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Senshi Matto &mdash; Competições de Jiu-Jitsu</title>
+
+	<!-- imagem aba d pagina -->
+	<link rel="shortcut icon" href="">
+	
+	<!-- css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body>
+	<!-- menu -->
+	<header role="banner" id="fh5co-header">
+		<div class="fluid-container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header">
+		
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+					<a class="navbar-brand" href="index.php">Senshi Matto</a> 
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="#" data-nav-section="home"><span>Home</span></a></li>
+						<li><a href="#" data-nav-section="services"><span>Quem Somos</span></a></li>
+						<li><a href="#" data-nav-section="explore"><span>Competições</span></a></li>
+						<li><a href="#" data-nav-section="testimony"><span>Filiações</span></a></li>
+						<li><a href="#" data-nav-section="pricing"><span>Ranking</span></a></li>
+						<li><a href="#" data-nav-section="team"><span>Perguntas</span></a></li>
+						<li><a href="#" data-nav-section="blog"><span>Fale Conosco</span></a></li>
+						<li class="call-to-action"><a class="sign-up" href="#"><span>Entrar</span></a></li>
+						<li class="call-to-action"><a class="log-in" href="#"><span>Registrar</span></a></li>
+					</ul>
+				</div>
+			</nav>
+	  </div>
+	</header>
+	<!-- // menu -->
+
+	<section id="fh5co-home" data-section="home" style="background-image: url(images/banner/banner01.jpg); height: 800px;" data-stellar-background-ratio="0.5">
+		<div class="gradient"></div>
+		<div class="container">
+			<div class="text-wrap">
+				<div class="text-inner">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<h1 class="to-animate">Controle de Competições <span>Jiu-Jitsu</span></h1>
+							<h2 class="to-animate">Maior Controle e segurança para seu campeonato</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="fh5co-services" data-section="services">
+		<div class="fh5co-services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate"><span>Competições em destaque</span></h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove. </h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3 text-center">
+						<div class="box-services">
+								<span><img src="images/destaque/destaque01.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Hand-crafted with Detailed</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>
+
+							</div>
+						</div>
+
+						<div class="box-services">
+								<span><img src="images/destaque/destaque02.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Light and Fast</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="box-services">
+								<span><img src="images/destaque/destaque03.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Award-winning Landing Page</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>
+							</div>
+						</div>
+
+						<div class="box-services">
+								<span><img src="images/destaque/destaque04.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Sleek and Smooth Animation</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="box-services">
+								<span><img src="images/destaque/destaque05.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Satisfied &amp; Happy Clients</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>	
+							</div>
+						</div>
+
+						<div class="box-services">
+								<span><img src="images/destaque/destaque06.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Looks Amazing on Devices</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>	
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="box-services">
+								<span><img src="images/destaque/destaque07.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>24/7 Help &amp; Support</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>	
+							</div>
+						</div>
+
+						<div class="box-services">
+								<span><img src="images/destaque/destaque08.jpg" alt=""></span>
+							<div class="fh5co-post to-animate">
+								<br>
+								<h3>Secure Account</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								<button type="button" class="btn btn-success">Inscrever-se</button>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<section id="fh5co-team" class="fh5co-bg-color" data-section="team">
+		<div class="fh5co-team">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate"><span>Nossa Equipe</span></h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove. </h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="team-box text-center to-animate-2">
+							<div class="user"><img class="img-reponsive" src="images/equipe/equipe01.png" alt="Roger Garfield"></div>
+							<h3>Roger Garfield</h3>
+							<span class="position">Co-Founder, Lead Developer</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<ul class="social-media">
+								<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+								<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+								<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+								<li><a href="#" class="codepen"><i class="icon-codepen"></i></a></li>
+								<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="team-box text-center to-animate-2">
+							<div class="user"><img class="img-reponsive" src="images/equipe/equipe01.png" alt="Roger Garfield"></div>
+							<h3>Kevin Steve</h3>
+							<span class="position">Co-Founder, Product Designer</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<ul class="social-media">
+								<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+								<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+								<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+								<li><a href="#" class="codepen"><i class="icon-codepen"></i></a></li>
+								<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="team-box text-center to-animate-2">
+							<div class="user"><img class="img-reponsive" src="images/equipe/equipe01.png" alt="Roger Garfield"></div>
+							<h3>Ross Standford</h3>
+							<span class="position">Full Stack Developer</span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<ul class="social-media">
+								<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+								<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+								<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+								<li><a href="#" class="codepen"><i class="icon-codepen"></i></a></li>
+								<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="fh5co-trusted" data-section="trusted">
+		<div class="fh5co-trusted">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate"><span>Equipes Parceiras</span></h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">We’re trusted by these popular companies</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					 <div class="col-md-2 col-sm-3 col-xs-6 col-sm-offset-0 col-md-offset-1">
+					 	<div class="partner-logo to-animate-2">
+					 		<img src="images/patrocinio/patrocinio01.jpg" alt="Partners" class="img-responsive">
+					 	</div>
+					 </div>
+				    <div class="col-md-2 col-sm-3 col-xs-6">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/patrocinio/patrocinio02.jpg" alt="Partners" class="img-responsive">
+						</div>
+				    </div>
+				    <div class="col-md-2 col-sm-3 col-xs-6">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/patrocinio/patrocinio03.jpg" alt="Partners" class="img-responsive">
+				    	</div>
+				    </div>
+				    <div class="col-md-2 col-sm-3 col-xs-6">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/patrocinio/patrocinio04.jpg" alt="Partners" class="img-responsive">
+				    	</div>
+				    </div>
+				    <div class="col-md-2 col-sm-12 col-xs-12">
+				    	<div class="partner-logo to-animate-2">
+				    		<img src="images/patrocinio/patrocinio05.jpg" alt="Partners" class="img-responsive">
+				    	</div>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 to-animate">
+					<h3 class="section-title">Sobre Nós</h3>
+					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics.</p>
+					<p class="copy-right">&copy; 2019 Senshi Matto - Competições. <br>Todos os direitos são reservados. <br>
+						Designed by <a href="" target="_blank">Daniel Gomes</a>
+					</p>
+				</div>
+
+				<div class="col-md-4 to-animate">
+					<h3 class="section-title">Our Address</h3>
+					<ul class="contact-info">
+						<li><i class="icon-map-marker"></i>198 West 21th Street, Suite 721 New York NY 10016</li>
+						<li><i class="icon-phone"></i>+ 1235 2355 98</li>
+						<li><i class="icon-envelope"></i><a href="#">info@yoursite.com</a></li>
+						<li><i class="icon-globe2"></i><a href="#">www.yoursite.com</a></li>
+					</ul>
+					<h3 class="section-title">Connect with Us</h3>
+					<ul class="social-media">
+						<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+						<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+						<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+						<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 to-animate">
+					<h3 class="section-title">Drop us a line</h3>
+					<form class="contact-form">
+						<div class="form-group">
+							<label for="name" class="sr-only">Name</label>
+							<input type="name" class="form-control" id="name" placeholder="Name">
+						</div>
+						<div class="form-group">
+							<label for="email" class="sr-only">Email</label>
+							<input type="email" class="form-control" id="email" placeholder="Email">
+						</div>
+						<div class="form-group">
+							<label for="message" class="sr-only">Message</label>
+							<textarea class="form-control" id="message" rows="7" placeholder="Message"></textarea>
+						</div>
+						<div class="form-group">
+							<input type="submit" id="btn-submit" class="btn btn-send-message btn-md" value="Send Message">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src="js/google_map.js"></script>
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+	</body>
+</html>
+
